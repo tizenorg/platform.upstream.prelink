6f483fc751257ab9348faf557a145f758462ce04
Anas Nashif <anas.nashif@intel.com>
2012-11-06 07:17:25 -0800

    fix copydtneeded

--- testsuite/functions.sh
+++ testsuite/functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-CC="${CC:-gcc} ${LINKOPTS}"
-CCLINK=${CCLINK:-${CC} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`}
-CXX="${CXX:-g++} ${LINKOPTS}"
-CXXLINK=${CXXLINK:-${CXX} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`}
+CC="${CC:-gcc} ${LINKOPTS} -Wl,--copy-dt-needed-entries" ; echo "CC=$CC"
+CCLINK="${CCLINK:-${CC} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`} -Wl,--copy-dt-needed-entries"
+CXX="${CXX:-g++} ${LINKOPTS} -Wl,--copy-dt-needed-entries" ; echo "CXX=$CXX"
+CXXLINK="${CXXLINK:-${CXX} -Wl,--dynamic-linker=`echo ./ld*.so.*[0-9]`} -Wl,--copy-dt-needed-entries"
 PRELINK=${PRELINK:-../src/prelink -c ./prelink.conf -C ./prelink.cache --ld-library-path=. --dynamic-linker=`echo ./ld*.so.*[0-9]`}
 srcdir=${srcdir:-`dirname $0`}
 savelibs() {
